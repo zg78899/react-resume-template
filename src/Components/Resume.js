24f4e693e2f6fa79b6ae08e8eb131a6ff3e43b20
kim jae hun <zg788990@gmail.com>
2021-01-12 01:05:42 +0900

    firstcommit

---
+++ src/Components/Resume.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+
+class Resume extends Component {
+  render() {
+    if (this.props.data) {
+      var skillmessage = this.props.data.skillmessage;
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
+          </div>
+        );
+      });
+      var project = this.props.data.project.map(function (project) {
+        return (
+          <div key={project.company}>
+            <h3>{project.company}</h3>
+            <p className="info">
+              {project.title}
+              <span>&bull;</span> <em className="date">{project.years}</em>
+            </p>
+            <p>{project.description1}</p>
+            <p>{project.description2}</p>
+            <p>{project.description3}</p>
+          </div>
+        );
+      });
+      var skills = this.props.data.skills.map(function (skills) {
+        var className = "bar-expand " + skills.name.toLowerCase();
+        return (
+          <li key={skills.name}>
+            <span style={{ width: skills.level }} className={className}></span>
+            <em>{skills.name}</em>
+          </li>
+        );
+      });
+    }
+
+    return (
+      <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">{education}</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Project</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">{project}</div>
+        </div>
+
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <p>{skillmessage}</p>
+
+            <div className="bars">
+              <ul className="skills">{skills}</ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Resume;
